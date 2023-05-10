52b140a9dc698af9c1bd4da8aa62c8335cbe5714
meppu <meppu@proton.me>
2023-05-10 20:42:44 +0300

    add email worker

---
+++ email/src/index.ts
@@ -0,0 +1,52 @@
+import { ForwardableEmailMessage } from "@cloudflare/workers-types";
+
+// Replace with your own ID
+const USER_ID: string = "1013270483560579165";
+
+export interface Env {
+  WEBHOOK_URL: string;
+}
+
+export default {
+  async email(message: ForwardableEmailMessage, env: Env, _ctx: any) {
+    const embed = embedBuilder(message);
+
+    await fetch(env.WEBHOOK_URL, {
+      method: "POST",
+      body: JSON.stringify(embed),
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+    });
+
+    await message.forward("meppu@proton.me");
+  },
+};
+
+// Build discord embed from email message
+function embedBuilder(message: ForwardableEmailMessage): object {
+  return {
+    content: `Hey <@${USER_ID}>!`,
+    embeds: [
+      {
+        title: "Received An Email",
+        color: 0xffd6fe,
+        fields: [
+          {
+            name: "From",
+            value: `\`${message.from}\``,
+            inline: true,
+          },
+          {
+            name: "To",
+            value: `\`${message.to}\``,
+            inline: true,
+          },
+        ],
+        footer: {
+          text: `Raw size is ${message.rawSize}`,
+        },
+      },
+    ],
+  };
+}
